0ff6acb98ffd6bc4c49000516b2ecc88b399c822
Nishant Mohan <nishant040305@gmail.com>
2025-12-16 20:46:48 +0530

    initial commit

---
+++ apps/web-frontend/android/app/src/main/java/com/stellariumlabs/stellaweb/MainActivity.java
@@ -0,0 +1,5 @@
+package com.stellariumlabs.stellaweb;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/web-frontend/public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "stellarium-pwa-v1";
+
+const CORE_ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      cached => cached || fetch(event.request)
+    )
+  );
+});
